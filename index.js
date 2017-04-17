29208865e4c5d700327c72104aef3e96097e1a2c
nqdai1992 <nguyendai1992@gmail.com>
2017-04-18 00:35:49 +0700

    Uninstal ipware

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * Created by Dainguyen on 17/04/2017.
  */
 const os = require('os');
-const getIP = require('ipware')().get_ip;
 const express = require('express');
 const port = process.env.PORT||5000;
 
@@ -11,9 +10,12 @@ app.get('/', (req, res) => {
    res.send('Hello world!')
 });
 app.get('/api/whoami/', (req, res)=>{
-    var ipInfo = getIP(req);
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
     var jsonResult = {
-        'ipAddress': ipInfo.clientIp.split(':')[3],
+        'ipAddress': ip.split(':')[3],
         'Language': req.headers["accept-language"].split(',')[0],
         'Software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
     }
